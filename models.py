cc2d977748c9ca5bff85f358be7766f04e97b84a
albertgilmorera <contacto@elbunkersa.com>
2025-04-03 15:25:38 +0200

    Primera subida

---
+++ models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class Producto(models.Model):
+    TIPO_CHOICES = [
+        ('raton', 'Ratón'),
+        ('teclado', 'Teclado'),
+        ('mando', 'Mando'),
+        ('casco', 'Casco'),
+        ('monitor', 'Monitor'),
+        # Puedes agregar más tipos según necesites
+    ]
+
+    nombre = models.CharField(max_length=100)
+    tipo = models.CharField(max_length=20, choices=TIPO_CHOICES)
+    imagen_url = models.URLField(verbose_name="URL de la imagen")
+    afiliado_url = models.URLField(verbose_name="URL de afiliado")
+    descripcion = models.TextField(verbose_name="Descripción (por qué recomiendas el producto)")
+
+    def __str__(self):
+        return self.nombre
